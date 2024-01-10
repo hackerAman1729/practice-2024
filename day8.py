47ccc69c82dfa2418d25f0a00dbd47e7c0ec2417
hackerAman1729 <104302829+hackerAman1729@users.noreply.github.com>
2024-01-10 16:46:10 +0000

    ques 2

--- day8.py
+++ day8.py
@@ -7,3 +7,28 @@ def is_fib(n):
 
 print(is_fib(8))  
 print(is_fib(15)) 
+
+# q2
+
+def merge_sort(arr1, arr2):
+    merged_arr = []
+    i = 0
+    j = 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            merged_arr.append(arr1[i])
+            i += 1
+        else:
+            merged_arr.append(arr2[j])
+            j += 1
+    while i < len(arr1):
+        merged_arr.append(arr1[i])
+        i += 1
+    while j < len(arr2):
+        merged_arr.append(arr2[j])
+        j += 1
+    return merged_arr
+
+arr1 = [1, 3, 5, 7]
+arr2 = [2, 4, 6, 8]
+   
